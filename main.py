cd0ac28d80d212bb9b0b748b231adf4396b08646
keeganleary <leary.keegan@gmail.com>
2020-11-28 15:07:47 -0800

    testing cron

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from selenium import webdriver
 from datetime import datetime
+import os
 
-print(f"{datetime.now()}: hello world!")
+
+def write_file(filename, data):
+    if os.path.isfile(filename):
+        with open(filename, "a") as f:
+            f.write('\n', + data)
+    else:
+        with open(filename, "w") as f:
+            f.write(data)
+
+
+def print_time():
+    now = datetime.now()
+    data = "Current Time: " + now
+    return data
+
+
+write_file('test.txt', print_time())
+
+
