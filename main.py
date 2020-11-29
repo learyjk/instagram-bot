e1e656b855a21fdbe274fdf93df3d7cf451ca010
keeganleary <leary.keegan@gmail.com>
2020-11-29 08:44:50 -0800

    headless chrome

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pyvirtualdisplay import Display
 from datetime import datetime
 import os
 
+CHROME_DRIVER_PATH_LOCAL = "/Users/keeganleary/Development/chromedriver"
+CHROME_DRIVER_PATH_VPS = "/home/keegadmin/chrome-driver"
+NUM_DAILY_ACCOUNTS_TO_FOLLOW = 100
+NUM_DAILY_ACCOUNTS_TO_UNFOLLOW = 15
+STARTING_HOUR = 8
 
-def write_file(filename, data):
+
+def get_accounts_to_follow(target_account):
+    pass
+
+
+def write_log(filename, data):
     if os.path.isfile(filename):
         with open(filename, "a") as f:
             f.write('\n' + data)
@@ -18,6 +30,19 @@ def print_time():
     return data
 
 
-write_file('test.txt', print_time())
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(CHROME_DRIVER_PATH_VPS, options=options)
+
+driver.get("https://google.com/")
+print(driver.title)
+
+driver.quit()
+
+
+
+
+
+write_log('test.txt', print_time())
 
 
